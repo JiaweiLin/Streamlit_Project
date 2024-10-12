76cf56756a534575751317ef1ef541766e119a43
Lin Jiawei <jiaweilin02@gmail.com>
2024-10-12 17:17:05 +0800

    Initial commit
    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+# Set up and run this Streamlit App
+import streamlit as st
+# from helper_functions import llm # <--- Not needed anymore. The helper function is now directly called by `customer_query_handler` 🆕
+from logics.customer_query_handler import process_user_message
+from helper_functions.utility import check_password
+
+# region <--------- Streamlit App Configuration --------->
+st.set_page_config(
+    layout="centered",
+    page_title="My Streamlit App"
+)
+# endregion <--------- Streamlit App Configuration --------->
+
+st.title("Streamlit App")
+
+# Check if the password is correct.  
+if not check_password():  
+    st.stop()
+
+form = st.form(key="form")
+form.subheader("Prompt")
+
+user_prompt = form.text_area("Enter your prompt here", height=200)
+
+if form.form_submit_button("Submit"):
+    st.toast(f"User Input Submitted - {user_prompt}")
+    response = process_user_message(user_prompt) #<--- This calls the `process_user_message` function that we have created 🆕
+    st.write(response)
+    print(f"User Input is {user_prompt}")
